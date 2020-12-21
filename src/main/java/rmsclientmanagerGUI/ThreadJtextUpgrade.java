395a59f1a9861c60996f565433ece91d681fb660
L.RUSSO62 <l.russo62@studenti.unisa.it>
2020-12-21 12:30:39 +0100

    Bug fixed

--- src/main/java/rmsclientmanagerGUI/ThreadJtextUpgrade.java
+++ src/main/java/rmsclientmanagerGUI/ThreadJtextUpgrade.java
@@ -3,12 +3,15 @@ package rmsclientmanagerGUI;
 import rmsclientmanager.*;
 
 import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.util.Map;
 
 public class ThreadJtextUpgrade implements Runnable{
     private Map<String,StringObject> outputjtext;
     private JTextArea jTextArea;
     private String deviceSelected;
+    private BufferedReader bufferedReader;
 
     public ThreadJtextUpgrade(Map<String, StringObject> outputjtext, JTextArea jTextArea, String deviceSelected) {
         this.outputjtext = outputjtext;
@@ -24,15 +27,22 @@ public class ThreadJtextUpgrade implements Runnable{
         this.outputjtext = outputjtext;
     }
 
+    public void setBufferedReader(BufferedReader bufferedReader) {
+        this.bufferedReader = bufferedReader;
+    }
+
     @Override
     public void run() {
         while(true){
-            jTextArea.append(outputjtext.get(deviceSelected).getOut());
             try {
-                Thread.sleep(60000);
-            } catch (InterruptedException e) {
+                String action=bufferedReader.readLine();
+                if(action.contains("monitoringvalue")) {
+                    jTextArea.append(outputjtext.get(deviceSelected).getOut());
+                }
+            } catch (IOException e) {
                 e.printStackTrace();
             }
+
         }
     }
 }
