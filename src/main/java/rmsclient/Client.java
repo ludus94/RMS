27278a1cf1761477bdfe9ebc90835f420356e539
L.RUSSO62 <l.russo62@studenti.unisa.it>
2021-01-15 12:40:16 +0100

    Bug fixed

--- src/main/java/rmsclient/Client.java
+++ src/main/java/rmsclient/Client.java
@@ -13,7 +13,7 @@ import java.net.*;
 public class Client {
     private Socket sock;
     private int port=33333;
-    private static final String address="62.98.33.48";
+    private static final String address="62.98.16.73";
     private MonitoringValue mv;
 
     /***
--- src/main/java/rmsclientmanager/ClientManager.java
+++ src/main/java/rmsclientmanager/ClientManager.java
@@ -20,7 +20,7 @@ public class ClientManager implements Runnable{
 
     private Socket sock;
     private int port=33333;
-    private static final String address="62.98.33.48";
+    private static final String address="62.98.16.73";
     private ArrayList<String> devicesList;
     private ArrayList<String> olddevicelist;
     private String username;
